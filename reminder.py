3f3df58589a4f4ea92d9c0371b0fe12ee6e495a5
Jayavanthsanaboina <jayavanthsanaboina@gmail.com>
2026-03-15 18:37:02 +0530

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,30 +1,19 @@
-import schedule
 import time
-from medication import get_medications
-
-
-def send_reminder(medicine):
-
-    print(f"Reminder: Time to take your medicine - {medicine}")
-
-
-def schedule_reminders():
-
-    meds = get_medications()
-
-    for med in meds:
-
-        name = med[1]
-        med_time = med[2]
-
-        schedule.every().day.at(med_time).do(send_reminder,name)
-
+from database import get_medications
 
 def start_reminder_service():
 
-    schedule_reminders()
-
     while True:
 
-        schedule.run_pending()
-        time.sleep(1)
+        current_time = time.strftime("%H:%M")
+
+        meds = get_medications()
+
+        for med in meds:
+
+            name,time_med = med
+
+            if current_time == time_med:
+                print(f"Reminder: Take {name}")
+
+        time.sleep(60)
