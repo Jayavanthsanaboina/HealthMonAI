bd321c45a6dd580f14c16a631428898c024855f5
Jayavanthsanaboina <jayavanthsanaboina@gmail.com>
2026-03-21 21:42:20 +0530

    Update medication.py

--- medication.py
+++ medication.py
@@ -6,3 +6,12 @@ def add_new_medication(name,time):
 def list_medications():
     meds = get_medications()
     return meds
+interaction_pairs = [
+    ("Aspirin", "Ibuprofen"),
+    ("Paracetamol", "Alcohol")
+]
+
+def check_interaction(med1, med2):
+    if (med1, med2) in interaction_pairs or (med2, med1) in interaction_pairs:
+        return "⚠ Possible interaction detected"
+    return "No known interaction"
