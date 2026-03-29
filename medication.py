931e484420c8ffa842c1731db95261b02e304365
Jayavanthsanaboina <jayavanthsanaboina@gmail.com>
2026-03-29 15:19:15 +0530

    Update medication.py

--- medication.py
+++ medication.py
@@ -15,3 +15,11 @@ def check_interaction(med1, med2):
     if (med1, med2) in interaction_pairs or (med2, med1) in interaction_pairs:
         return "⚠ Possible interaction detected"
     return "No known interaction"
+indian_medicines = {
+    "Dolo 650": "Fever, pain relief",
+    "Crocin": "Fever",
+    "Zincovit": "Vitamin supplement"
+}
+
+def get_medicine_info(name):
+    return indian_medicines.get(name, "No info available")
